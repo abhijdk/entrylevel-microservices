a4b85f1f13c5b22f1c1fc70d2ee3ac391b06ad79
Abhinna Sundar Behuria <abhinnabehuria@outlook.com>
2025-06-08 20:15:58 +0530

    Added Feign client for CurrencyExchange service to simplify inter-service communication

--- currency-conversion-service/src/main/java/com/abhi/currency_conversion_service/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/abhi/currency_conversion_service/controller/CurrencyConversionController.java
@@ -1,6 +1,8 @@
 package com.abhi.currency_conversion_service.controller;
 
 import com.abhi.currency_conversion_service.entity.CurrencyConversion;
+import com.abhi.currency_conversion_service.feign.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,29 +14,65 @@ import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeProxy currencyExchangeProxy;
+
 //http://localhost:8100/currency-conversion/from/{from}/to/{to}/quantity/{quantity}
-    @GetMapping ("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversion calculateCurrencyCOnversion(
-                            @PathVariable String from,
-                            @PathVariable String to,
-                            @PathVariable BigDecimal quantity
+@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+public CurrencyConversion calculateCurrencyConversion(
+        @PathVariable String from,
+        @PathVariable String to,
+        @PathVariable BigDecimal quantity) {
 
-                                                          ){
-        HashMap<String,String> uriVariable = new HashMap<>();
-        uriVariable.put("from",from);
-        uriVariable.put("to",to);
+    HashMap<String, String> uriVariables = new HashMap<>();
+    uriVariables.put("from", from);
+    uriVariables.put("to", to);
 
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/USD/to/INR",
-                CurrencyConversion.class,
-                uriVariable
+    ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+            "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+            CurrencyConversion.class,
+            uriVariables
+    );
 
+    CurrencyConversion currencyConversion = responseEntity.getBody();
+
+    BigDecimal totalCalculatedAmount = quantity.multiply(currencyConversion.getConversionMultiple());
+
+    return new CurrencyConversion(
+            currencyConversion.getId(),
+            from,
+            to,
+            currencyConversion.getConversionMultiple(),
+            quantity,
+            totalCalculatedAmount,
+            currencyConversion.getEnvironment()
+    );
+}
+
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity) {
+
+
+        CurrencyConversion currencyConversion = currencyExchangeProxy.retriveExchange(from,to);
+
+        BigDecimal totalCalculatedAmount = quantity.multiply(currencyConversion.getConversionMultiple());
+
+        return new CurrencyConversion(
+                currencyConversion.getId(),
+                from,
+                to,
+                currencyConversion.getConversionMultiple(),
+                quantity,
+                totalCalculatedAmount,
+                currencyConversion.getEnvironment()+" "+"feign"
         );
-
-        CurrencyConversion currencyConversion = responseEntity.getBody();
-
-BigDecimal totalCalculatedAmount=quantity.multiply(currencyConversion.getConversionMultiple());
-        return new CurrencyConversion(currencyConversion.getId(),from,to, currencyConversion.getConversionMultiple(),quantity,totalCalculatedAmount,currencyConversion.getEnvironment());
     }
+
 
 
 }
